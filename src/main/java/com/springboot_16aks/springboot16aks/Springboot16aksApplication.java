a639e8128c2d4790e3efa1e5879f5bd238f7cc30
asarkar <asarkar@promnetwork.com>
2019-04-03 15:38:02 -0400

    Initial commit

---
+++ src/main/java/com/springboot_16aks/springboot16aks/Springboot16aksApplication.java
@@ -0,0 +1,13 @@
+package com.springboot_16aks.springboot16aks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot16aksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot16aksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot_16aks/springboot16aks/Springboot16aksApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot_16aks.springboot16aks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot16aksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
